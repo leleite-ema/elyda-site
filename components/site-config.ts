7e5c9bb64e8f2a2231e789e36ae345ea2a90c927
Leandro Leite <le.leite@gmail.com>
2026-02-15 20:00:28 -0300

    fix site config keys

--- components/site-config.ts
+++ components/site-config.ts
@@ -2,6 +2,7 @@ export const site = {
   name: "Élyda Maele",
   tagline: "Nutricionista Clínica Online",
   city: "São Paulo - SP",
+  crn: "CRN3 84671",
   whatsappNumber: "+5511985494837", // <-- TROQUE AQUI (DDI+DDD+NUMERO)
   whatsappMessage:
     "Olá! Gostaria de agendar uma consulta online com a Nutricionista Élyda Maele (CRN3 84671). Meu objetivo é: ____. Prefiro o período: ____.",
